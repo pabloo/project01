d92221e7897f9492d55a4de9a866a6045b5d4a20
kursant1 <kursant1@kursant1-pc.319.wroclaw.altkom.local>
2014-08-13 11:52:45 +0200

    1

--- src/main/java/pl/altkom/util/validation/OdbiorcaValidator.java
+++ src/main/java/pl/altkom/util/validation/OdbiorcaValidator.java
@@ -8,16 +8,24 @@ import pl.altkom.util.StringUtils;
 public class OdbiorcaValidator {
 	
 	
-	
 	public void validate(Odbiorca odbiorca, Errors error){
+		
+		
 		if(StringUtils.isEmpty(odbiorca.getImie())){
 			error.reject("odbiorca.imie", "Pole wymagane");
+		}
+		
+		if(Character.isLowerCase(odbiorca.getImie().charAt(0))){
+			error.reject("odbiorca.imie", "Imie z duzej litery");
 		}
 		
 		if(StringUtils.isEmpty(odbiorca.getNazwisko())){
 			error.reject("odbiorca.nazwisko", "Pole wymagane");
 		}
-		
+
+		if(Character.isLowerCase(odbiorca.getNazwisko().charAt(0))){
+			error.reject("odbiorca.nazwisko", "Nazwisko z duzej litery");
+		}
 		if(StringUtils.isEmpty(odbiorca.getMail())){
 			error.reject("odbiorca.mail", "Pole wymagane");
 		}
